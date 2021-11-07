1ddc69688ff2e8e678c5ed654106b7dc3dbc46ee
Mars Liang <marsfantasy@gmail.com>
2021-11-07 22:00:51 +0800

    Update Index.cshtml.cs

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -18,8 +18,8 @@ namespace LineBot.Pages
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
-            token = "U85pqiuKCkc3/nKv7pV87JIdT7Uvm6RhqQRHSxCWTpA4W8ysmTFRZWWYK/xRZI86rtxa89Wuptb3oMWszfDeaDYbtlE/ndSzTSK5BLJWPAed/cBMVAvoLw9kKPoWZuaz7DwOBlhwN6+Cmg637JCLSAdB04t89/1O/w1cDnyilFU=";
-            AdminUserId = "marsfantasy";
+
+            
         }
 
         public IActionResult OnGet()
