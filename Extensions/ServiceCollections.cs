9b9c1f880e51491eb92e32e12f494180f3086c75
marsfantasy <marsfantasy@gmail.com>
2021-11-07 21:22:18 +0800

    加入專案檔案。

---
+++ Extensions/ServiceCollections.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LineBot.Extensions
+{
+    public static class ServiceCollections
+    {
+        public static void AddLineBotSDK(this IServiceCollection services, IConfiguration Configuration)
+        {
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using isRock.LineBot;
+
+namespace LineBot.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private string token;
+        private string AdminUserId;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            token = "U85pqiuKCkc3/nKv7pV87JIdT7Uvm6RhqQRHSxCWTpA4W8ysmTFRZWWYK/xRZI86rtxa89Wuptb3oMWszfDeaDYbtlE/ndSzTSK5BLJWPAed/cBMVAvoLw9kKPoWZuaz7DwOBlhwN6+Cmg637JCLSAdB04t89/1O/w1cDnyilFU=";
+            AdminUserId = "marsfantasy";
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+    }
+}
